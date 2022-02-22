96b60e95b8754b9f04117769fbf1789127081a9d
Sahitya Neupane <neupanesahitya1@gmail.com>
2022-02-22 15:16:16 +0545

    this is my first change in my portfolio

---
+++ script/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const menu = document.querySelector('.menu');
+const closeMenu = document.querySelector('.close');
+const ulBar = document.querySelector('.hig-class');
+const navBar = document.querySelector('.nav-bar');
+menu.addEventListener('click', function () {
+  menu.classList.toggle('v-class');
+  closeMenu.classList.toggle('v-class');
+  ulBar.classList.toggle('v-class');
+  navBar.classList.toggle('h-nav');
+});
